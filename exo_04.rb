022cf31f8006e637cc79415a2d9f7af4483d4c56
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-01-17 14:23:55 +0100

    Grande aide de Karine

--- exo_04.rb
+++ exo_04.rb
@@ -4,8 +4,4 @@
 puts "Quelle est ton année de naissance ?"
 user_age = gets.chomp.to_i
 
-100.times do |user_age|
-    puts user_age + 1
-end 
-
-puts "Tu auras 100 ans en #{user_age} !"
+puts "Tu auras 100 ans en #{user_age + 100} !"
