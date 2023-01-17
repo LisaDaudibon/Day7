45deaa4b7a118dd59a09ffa83f7cd2cdaaa9890f
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-01-17 16:23:36 +0100

    Exo 12

--- exo_12.rb
+++ exo_12.rb
@@ -2,16 +2,19 @@
 # ruby exo_12.rb
 
 puts "Quelle est ton année de naissance ?"
-user_age = gets.chomp.to_i
+user_year = gets.chomp.to_i
 a = 2023
-i = 0
-c = (2023 - user_age) * 1/2
-puts c
 
-while user_age <= a
-    puts "#{user_age}"
-    puts " Il y a #{a - user_age} ans, tu avais #{i} ans !"
-    user_age += 1
+c = (2023 - user_year) * 1/2 #la moitié de ton âge
+#puts c
+
+i = 0
+while user_year < a
+    user_year += 1
     i += 1
-     
+    if i == c
+        puts "Il y a #{a - user_year} ans, tu avais la moitié de l'âge que tu as aujourd'hui ! "
+    else 
+        puts " Il y a #{a - user_year} ans, tu avais #{i} ans !"
+    end
 end
